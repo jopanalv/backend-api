46052f06a360fe39d798f0abd3fa46a68f9061e2
jopanalv <jopan.alv@gmail.com>
2021-12-06 20:27:29 +0700

    adding create product function

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class ProductController extends Controller
 {
@@ -30,7 +31,42 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validate = $request->validate([
+            'name' => 'required|string',
+            'slug' => 'required|string',
+            'image' => 'required|string',
+            // 'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:4096',
+            'price' => 'required|integer',
+            'stock' => 'required|integer',
+            'specification' => 'required|string',
+            'function' => 'required|string',
+            'utility' => 'required|string',
+            'commodity' => 'required|string'
+        ]);
+
+        // $image = $request->file('image');
+        // $image->storeAs('public/images/',$image->hashName());
+
+        $product = Product::create([
+            'name' => $validate['name'],
+            'slug' => $validate['slug'],
+            'image' => $validate['image'],
+            // 'image' => $image->hashName(),
+            'price' => $validate['price'],
+            'stock' => $validate['stock'],
+            'specification' => $validate['specification'],
+            'function' => $validate['function'],
+            'utility' => $validate['utility'],
+            'commodity' => $validate['commodity']
+            
+        ]);
+
+        $response = [
+            'message' => 'Create product Successful',
+            'data' => $product,
+        ];
+
+        return response($response, 201);
     }
 
     /**
