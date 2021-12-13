513e250d6e0bf690aaccbb964d1414f45155d20f
jopanalv <jopan.alv@gmail.com>
2021-12-13 20:55:23 +0700

    adding delete user route

--- routes/api.php
+++ routes/api.php
@@ -28,13 +28,14 @@ Route::get('/product/{slug}', [ProductController::class, 'show']);
 Route::group(['middleware' => ['auth:sanctum']], function() {
     Route::post('/addproduct', [ProductController::class, 'store']);
     Route::post('/product/edit/{id}', [ProductController::class, 'update']);
-    Route::delete('/delete/{id}', [ProductController::class, 'destroy']);
+    Route::delete('/product/delete/{id}', [ProductController::class, 'destroy']);
 
     Route::post('/cart', [CartController::class, 'store']);
     Route::get('/listcart', [CartController::class, 'index']);
 
     Route::get('/listuser', [AuthController::class, 'listUser']);
     Route::post('/logout', [AuthController::class, 'logout']);
+    Route::delete('/user/delete/{id}', [AuthController::class, 'destroy']);
 });
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
