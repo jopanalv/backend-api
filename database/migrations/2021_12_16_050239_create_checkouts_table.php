9c12fc976a25efeab23e81dbd3a99f9b6f1b0402
jopanalv <jopan.alv@gmail.com>
2021-12-16 12:14:14 +0700

    creating models,migrations, controllers checkout

---
+++ database/migrations/2021_12_16_050239_create_checkouts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCheckoutsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('checkouts', function (Blueprint $table) {
+            $table->id();
+            $table->integer('cart_id')->unsigned();
+            $table->string('proof');
+            $table->string('status', ['waiting', 'proses', 'success', 'failed']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('checkouts');
+    }
+}
